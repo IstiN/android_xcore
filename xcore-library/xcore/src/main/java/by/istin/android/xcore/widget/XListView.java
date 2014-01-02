0babdf3291ab0f88897b24fd6334c4aa698e772d
IstiN <istin2007@gmail.com>
2014-01-03 00:23:33 +0300

    added xlistview for future

---
+++ xcore-library/xcore/src/main/java/by/istin/android/xcore/widget/XListView.java
@@ -0,0 +1,22 @@
+package by.istin.android.xcore.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class XListView extends ListView {
+
+    public XListView(Context context) {
+        super(context);
+    }
+
+    public XListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+}
